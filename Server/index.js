01c74bcecd1e05d80c30b210ab173f1674d9f8e9
Deepanshupal3510e <deepanshupal3510e@gmail.comm>
2025-03-22 11:21:46 +0530

    Update CORS configuration to use environment variable for frontend URL

--- Server/index.js
+++ Server/index.js
@@ -14,22 +14,10 @@ import ProductRouter from './routes/product.route.js';
 const app = express();
 app.use(cors({
     credentials : true,
-    origin : "https://onlineshopbydeepanshu.netlify.app"
+    origin : process.env.FRONTEND_URL
 
 }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://onlineshopbydeepanshu.netlify.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-  
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-  
-    next();
-  });
 
 app.use(express.json())
 app.use(cookieParser())
